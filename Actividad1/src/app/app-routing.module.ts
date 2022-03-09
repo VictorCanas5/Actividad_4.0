3a90334cb778d40baa1fd2a699560feb69f8c910
VictorCanas5 <victordcm@outlook.es>
2022-03-08 22:26:06 -0600

    Introduccion, index, login y register sin auth

--- Actividad1/src/app/app-routing.module.ts
+++ Actividad1/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/pages/index/index.component';
+import { LoginComponent } from './logeo/pages/login/login.component';
+import { RegisterComponent } from './logeo/pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+    {path:'', redirectTo: 'inicio', pathMatch:'full'},
+    {path:'inicio',component: IndexComponent},
+    {path:'login', component: LoginComponent},
+    
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Actividad1/src/app/app.module.ts
+++ Actividad1/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './Componentes/index/index.component';
-import { RegisterComponent } from './Componentes/register/register.component';
-import { LoginComponent } from './Componentes/login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/pages/index/index.component';
+import { RegisterComponent } from './logeo/pages/register/register.component';
+import { LoginComponent } from './logeo/pages/login/login.component';
+import { HeaderComponent } from './index/components/header/header.component';
+import { FooterComponent } from './index/components/footer/footer.component';
+
+
 
 
 @NgModule({
@@ -22,6 +25,7 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
