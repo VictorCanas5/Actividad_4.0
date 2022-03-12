866dfdc23f3231bdd543efae1a89effabbffc4c1
rivaldo1011 <roquerivaldo11@gmail.com>
2022-03-12 02:41:41 -0600

    Crud(sin terminar)

---
+++ Actividad_Adonis/app/Controllers/Http/CitasController.ts
@@ -0,0 +1,45 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Cita from 'App/Models/Cita';
+import Cita from 'App/Models/Cita';
+
+export default class CitasController {
+  public async index({}: HttpContextContract) 
+  {
+
+  }
+
+  public async create({request, response}: HttpContextContract)
+  {
+    let input = request.all();
+    await Cita.create(input);
+    return response.status(200).send({
+      res: true,
+      message: "categoria creada correctamente"
+    });
+  }
+
+  public async store({}: HttpContextContract) 
+  {
+  }
+
+  public async show({}: HttpContextContract) {
+
+  }
+  public async update({params, request, response}: HttpContextContract) 
+  {
+    await Cita.query().where('id_categoria', params.id).update(request.all());
+    return {
+      res: true,
+      message: "Cita modificada correctamente"
+    }
+  }
+
+  public async destroy({ params, request, response}: HttpContextContract) {
+    const cita = await Cita.findOrFail(params.id);
+    await cita.delete();
+    return {
+      res: true,
+      message: "Registro eliminado correctamente"
+    }
+  }
+}
